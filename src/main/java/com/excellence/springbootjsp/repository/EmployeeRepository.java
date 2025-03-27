74d27db4d25e02badb743cebaa90a605c5f93cd7
excellence <chintanpatel2013@icloud.com>
2025-03-27 20:39:03 +0530

    Create EmployeeRepository file for this application.

---
+++ src/main/java/com/excellence/springbootjsp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.excellence.springbootjsp.repository;
+
+import com.excellence.springbootjsp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("employeeRepository")
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
